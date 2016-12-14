52af5e450217aa6824af771abed9654d0ff7effa
perkinsung <perkinsung@yvtcdeimac.local>
2016-12-14 11:44:03 +0800

    Initial Commit

---
+++ iBeaconSensor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iBeaconSensor
+//
+//  Created by perkinsung on 2016/12/14.
+//  Copyright © 2016年 perkinsung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
